1dc5f80604295e0672f0e823c3abcbdad8d8d0e5
De-Anna <61568952+dclarke044@users.noreply.github.com>
2020-05-07 19:14:43 -0400

    final commit

--- Query.sql
+++ Query.sql
@@ -1,4 +1,4 @@
---- Data Analysis
+-------------------------- DATA ANALYSIS -----------------------------
 --- List the following details of each employee: employee number, last name, first name, sex, and salary
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 from employees as e
@@ -11,16 +11,12 @@ from employees as e
 where extract(year from e.hire_date) =1986;
 
 --- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name
-SELECT de.dept_no, d.dept_name, de.emp_no, e.last_name, e.first_name
-from employees as e
-INNER JOIN dept_emp as de
-on e.emp_no = de.emp_no
-INNER JOIN departments as d
-on de.dept_no = d.dept_no
-INNER JOIN titles as tl
-on e.title_id = tl.title_id
-WHERE tl.title = 'Manager';
-
+SELECT e.emp_no, e.last_name, e.first_name, dm.dept_no, d.dept_name
+from dept_manager as dm
+Inner JOIN employees as e 
+on dm.emp_no = e.emp_no
+Inner JOIN departments as d
+on d.dept_no = dm.dept_no;
 
 --- List the department of each employee with the following information: employee number, last name, first name, and department name
 SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
@@ -31,6 +27,10 @@ INNER JOIN departments as d
 on de.dept_no = d.dept_no;
 
 --- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B"
+SELECT e.first_name, e.last_name, e.sex
+from employees as e
+where e.first_name = 'Hercules'
+and e.last_name like 'B%';
 
 --- List all employees in the Sales department, including their employee number, last name, first name, and department name
 SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
@@ -49,3 +49,9 @@ on e.emp_no = de.emp_no
 INNER JOIN departments as d
 on de.dept_no = d.dept_no
 where d.dept_name in('Sales', 'Development'); 
+
+--- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+SELECT e.last_name, COUNT(e.last_name) as "Surname Tally"
+from employees as e
+GROUP BY e.last_name
+order by "Surname Tally" DESC;
